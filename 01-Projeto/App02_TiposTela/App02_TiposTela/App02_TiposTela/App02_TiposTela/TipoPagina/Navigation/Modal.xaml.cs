593726f3bd156856b04eb5dfd86581a6df853862
Paulo Santos | Casa <phs.developer@gmail.com>
2019-07-18 00:11:10 -0300

    **Aula 40. [ESTUDO] Navigation Page - Modal

--- 01-Projeto/App02_TiposTela/App02_TiposTela/App02_TiposTela/App02_TiposTela/TipoPagina/Navigation/Modal.xaml.cs
+++ 01-Projeto/App02_TiposTela/App02_TiposTela/App02_TiposTela/App02_TiposTela/TipoPagina/Navigation/Modal.xaml.cs
@@ -16,5 +16,18 @@ namespace App02_TiposTela.TipoPagina.Navigation
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Btn_fechar_Clicked(object sender, EventArgs e)
+        {
+            /********************************************
+             * Navigation.PopModalAsync(): Fecha a tela que foi aberta como uma modal
+             * Navigation.PopAsync(); => volta para a pagina anterior, igual a seta que fica na parte superior
+             * Navigation.PopToRootAsync(); => Volta para a primeira pagina adicionada na pilha de chamadas.
+             */
+
+           
+            Navigation.PopModalAsync();
+
+        }
+    }
 }
--- 01-Projeto/App02_TiposTela/App02_TiposTela/App02_TiposTela/App02_TiposTela/TipoPagina/Navigation/Pagina1.xaml.cs
+++ 01-Projeto/App02_TiposTela/App02_TiposTela/App02_TiposTela/App02_TiposTela/TipoPagina/Navigation/Pagina1.xaml.cs
@@ -9,25 +9,45 @@ using Xamarin.Forms.Xaml;
 
 namespace App02_TiposTela.TipoPagina.Navigation
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Pagina1 : ContentPage
-	{
-		public Pagina1 ()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina1 : ContentPage
+    {
+        public Pagina1()
+        {
+            InitializeComponent();
+        }
 
-        private void btn_mudar_click(object sender,EventArgs args)
+        private void btn_mudar_click(object sender, EventArgs args)
         {
             DisplayAlert("Aviso Sistema", "Envento chamado", "Entendi");
             Navigation.PushAsync(new Pagina2());
-           
+
         }
 
-        private void btn_chamarModal_click(object sender,EventArgs args)
+        private void btn_chamarModal_click(object sender, EventArgs args)
         {
-
+            Navigation.PushModalAsync(new Modal());
         }
 
+        private void Tbi_Ordenar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tbi_Ordenar.Icon.File.Equals("sort_descending.png"))
+                    tbi_Ordenar.Icon = "sort_ascending.png";
+
+                else
+                    tbi_Ordenar.Icon = "sort_descending.png";
+
+                DisplayAlert("Aviso Sistema", $"Ordenar: {(tbi_Ordenar.Icon.File.Contains("ascending")? "ascending": "descending")}", "Entendi");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
     }
 }
